54d11ea17c48632c48e7ec45836d911403fdb308
Sorcha Bowler <saoili@gmail.com>
2013-11-06 22:49:32 +0000

    updating functional test file

--- functional_tests.py
+++ functional_tests.py
@@ -1,21 +1,34 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_convert_from_one_to_another(self):
+        # Anita recently ran with some friends and was told that
+        # she ran 'a ten minute mile pace'. She normally thinks
+        # in kilometers per hour and wants to convert. Sorcha
+        # had told her about an app she was writing that did just that
+        # so she opened the app in a browser
+        self.browser.get('http://localhost:8000')
 
-# Anita recently ran with some friends and was told that
-# she ran 'a ten minute mile pace'. She normally thinks
-# in kilometers per hour and wants to convert. Sorcha
-# had told her about an app she was writing that did just that
-# so she opened the app in a browser
-browser.get('http://localhost:8000')
+        # she notices that the title of the webpage talks about running
+        # and converting
+        self.assertIn ('run', self.browser.title)
+        self.assertIn ('convert', self.browser.title)
+        self.fail('Finish the test!')
+        
+        # she sees a place to put in the minute mile value
 
-# she notices that the title of the webpage talks about running
-# and converting
-assert 'run' in browser.title
-assert 'converter' in browser.title
+        # she enters the minute mile value and clicks a button or hits enter
 
-# she sees a place to put in the minute mile value
-
-# she enters the minute mile value and clicks a button or hits enter
-
-# the website tells her how many kilometers per hour she ran
+        # the website tells her how many kilometers per hour she ran
+        
+if __name__ == '__main__':
+    unittest.main()
